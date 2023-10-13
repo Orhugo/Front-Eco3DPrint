86a5d4363bb224d183b448dcc75fb95aa36e8f29
Hugo Calzado <hcalzado@gmail.com>
2023-10-13 22:20:04 +0200

    Creada barra de navegación

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
 import React from "react";
-import SubirArchivo from "./views/SubirArchivo";
 import Home from "./views/Home";
 import VisualizarSTL from "./views/VisualizarSTL";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import UploadFile from "./views/UploadFile";
+import BarraNav from "./components/BarraNav";
 
 function App() {
   return (
-    <UploadFile />
-    // <Router>
-    //   <div>
-    //     <Routes>
-    //       <Route path="/" element={<Home />} />
-    //       <Route path="/subirArchivo" element={<SubirArchivo />} />
-    //       <Route path="/visualizarSTL" element={<VisualizarSTL />} />
-    //     </Routes>
-    //   </div>
-    // </Router>
+    <>
+      <BarraNav />
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/subirArchivo" element={<UploadFile />} />
+            <Route path="/visualizarSTL" element={<VisualizarSTL />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
