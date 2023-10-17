f1a68f440221af1d752ead57924e1805bda80147
Hugo <hcalgar@etsinf.upv.es>
2023-10-17 09:35:07 +0200

    Corregidos todos los nombres

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import Home from "./views/Home";
-import VisualizarSTL from "./views/STLVisualizer.jsx";
+import STLVisualizer from "./views/STLVisualizer.jsx";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import UploadFile from "./views/UploadFile";
-import BarraNav from "./components/NavBar.jsx";
+import NavBar from "./components/NavBar.jsx";
 
 function App() {
   return (
     <>
-      <BarraNav />
+      <NavBar />
       <Router>
         <div>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/subirArchivo" element={<UploadFile />} />
-            <Route path="/visualizarSTL" element={<VisualizarSTL />} />
+            <Route path="/visualizarSTL" element={<STLVisualizer />} />
           </Routes>
         </div>
       </Router>
