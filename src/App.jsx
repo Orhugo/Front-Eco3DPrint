a16b106d026b51e14f9bb7fae09bf796529d7654
Mirco Vella <mircovellainfo@gmail.com>
2023-10-21 17:27:18 +0200

    Model Information Page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
 import React from "react";
 import Home from "./views/Home";
-import VisualizarSTL from "./views/STLVisualizer";
+import STLVisualizer from "./views/STLVisualizer";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import UploadFile from "./views/UploadFile";
-import UserRegistration from "./components/UserRegistration";
-import ProfileConfig from "./components/ProfileConfig";
-import BarraNav from "./components/NavBar";
+import UserRegistration from "./views/UserRegistration";
+import ProfileConfig from "./views/ProfileConfig";
+import NavBar from "./components/NavBar";
+import UserLogin from "./views/UserLogin";
+import InfoModel from "./views/InfoModel"
 
 function App() {
   return (
     <>
-      <BarraNav />
+      
       <Router>
-        <div>
+      <NavBar />
+        <div style={{marginTop:"100px"}}>
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/subirArchivo" element={<UploadFile />} />
-            <Route path="/visualizarSTL" element={<VisualizarSTL />} />
-            <Route path="/UserRegistration" element={<UserRegistration />} />
-            <Route path="/ProfileConfig" element={<ProfileConfig />} />
+            <Route path="/Front-Eco3DPrint" element={<Home />} />
+            <Route path="/Front-Eco3DPrint/subirArchivo" element={<UploadFile />} />
+            <Route path="/Front-Eco3DPrint/visualizarSTL" element={<STLVisualizer />} />
+            <Route path="/Front-Eco3DPrint/UserRegistration" element={<UserRegistration />} />
+            <Route path="/Front-Eco3DPrint/UserLogin" element={<UserLogin />} />
+            <Route path="/Front-Eco3DPrint/ProfileConfig" element={<ProfileConfig />} />
+            <Route path="/Front-Eco3DPrint/InfoModel" element={<InfoModel />} />
           </Routes>
         </div>
       </Router>
--- src/views/STLVisualizer.jsx
+++ src/views/STLVisualizer.jsx
@@ -1,29 +1,53 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import React from "react";
 import { StlViewer } from "react-stl-viewer";
-
-const url = "/Face.stl";
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const style = {
+  position: "relative",
   top: 0,
   left: 0,
   width: "100vw",
   height: "100vh",
 };
 
+const buttonStyle = {
+  position: "absolute",
+  top: "80px",  
+  left: "50%",
+  transform: "translateX(-50%)",
+  padding: "15px 30px",
+  fontSize: "20px",
+  zIndex: 1,
+};
+
 export function VisualizarSTL() {
+  const { state } = useLocation();
+  const url = state ? state + ".stl" : "Face.stl";
+  const navigate = useNavigate();
+  const modelId = 2; //SHOULD RETRIEVE IT FROM DB
+
+  const handleButtonClick = () => {
+    navigate(`/Front-Eco3DPrint/InfoModel?id=${modelId}`);
+  };
+
   return (
-    <StlViewer 
-      style={style} 
-      orbitControls 
-      shadows 
-      url={url}
-      modelProps={{
-        color: "#0a6bc1",
-        positionX: 0,
-        positionY: 0,
-      }}
-    />
+    <div style={style} >
+      <StlViewer 
+        style={style} 
+        orbitControls 
+        shadows 
+        url={url}
+        modelProps={{
+          color: "#0a6bc1",
+          positionX: 0,
+          positionY: 0,
+        }}
+      />
+      <button style={buttonStyle} onClick={handleButtonClick}>
+        Visualizar Información
+      </button>
+    </div>
   );
 }
 
