9a37e740c65144f351c6d9789be0ad631707be2f
Parbulitos <cechavarne@gmail.com>
2023-11-06 18:28:59 +0100

    Styles depending on user logged applied

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,7 @@ export default {
         'dark': '0 3px 10px rgba(0, 0, 0, 0.5)',
         'darker': '0 5px 10px rgba(0, 0, 0, 0.75)',
         'content': '5px 0 15px rgba(0,0,0,0.75), -5px 0 15px rgba(0,0,0,0.75)',
+        'dark-spread': '0 3px 20px rgba(0, 0, 0, 0.5)',
       },
     },
     fontFamily: {
