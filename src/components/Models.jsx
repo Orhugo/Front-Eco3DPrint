e86365c78dc806e3b863d941bb33ec490b1573b9
Parbulitos <cechavarne@gmail.com>
2023-11-10 15:40:30 +0100

    Catalog shows model preview and navigates to its own STLVisualizer

--- src/components/Models.jsx
+++ src/components/Models.jsx
@@ -3,25 +3,17 @@ import './Models.css'
 import {StlViewer} from "react-stl-viewer";
 
 const Model = ({onClick, modelName, modelUrl}) => {
-    const [hovered, setHovered] = useState(false);
-
-    const handleModelInfoEnter = () => {
-        setHovered(true)
-    }
-    const handleModelInfoLeave = () => {
-        setHovered(false)
-    }
     return (
 
-        <div className="w-[18%] m-[1%] aspect-square hover:cursor-pointer transition hover:scale-[1.03] duration-300"
-             onClick={onClick} onMouseEnter={handleModelInfoEnter} onMouseLeave={handleModelInfoLeave}>
+        <div className="w-[18%] m-[1%] aspect-square hover:cursor-pointer transition hover:scale-[1.03] duration-300">
             <div className="w-full h-[70%] rounded-t-[15px] bg-white">
                 <StlViewer
                     orbitControls={false}
                     url={modelUrl}
                     modelProps={{
                         color: "#0a6bc1",
-                        scale: 1.5
+                        positionX:100,
+                        scale: 2.2
                     }}
                 />
             </div>
