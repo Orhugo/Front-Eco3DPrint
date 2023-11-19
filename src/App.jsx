dc0e14c4a5c831c6a23c7cde61cb2a312fccaaf4
Mirco Vella <mircovellainfo@gmail.com>
2023-11-19 12:18:19 +0100

    infomodel and download update

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,22 @@ import NavBar from "./components/NavBar";
 import Catalog from "./views/Catalog";
 import UserLogin from "./views/UserLogin";
 import Comment from "./views/Comment";
+<<<<<<< Updated upstream
 import BarraNav from "./components/NavBar";
 import InfoModel from "./views/InfoModel"
+=======
+import Tutorials from "./views/TutorialsCatalog";
+import TutorialHandler from "./views/TutorialHandler";
+import Profile from "./views/Profile";
+import Budget from "./views/Budget";
+import AuthorsProfile from "./views/AuthorsProfile";
+import InfoModel from "./views/InfoModel";
+>>>>>>> Stashed changes
 
 
 function App() {
   return (
+<<<<<<< Updated upstream
     <>
       
       <Router>
@@ -34,6 +44,31 @@ function App() {
         </div>
       </Router>
     </>
+=======
+      <div className="h-screen w-full flex justify-center overflow-scroll">
+        <Router>
+          <NavBar/>
+            <div className="w-full h-screen flex justify-center items-center">
+              <Routes>
+                <Route path="/Volume" element={<Home />} />
+                <Route path="/Volume/subirArchivo" element={<UploadFile />} />
+                <Route path="/Volume/visualizarSTL" element={<STLVisualizer />} />
+                <Route path="/Volume/catalogo" element={<Catalog />} />
+                <Route path="/Volume/UserRegistration" element={<UserRegistration />} />
+                <Route path="/Volume/UserLogin" element={<UserLogin />} />
+                <Route path="/Volume/ProfileConfig" element={<ProfileConfig />} />
+                <Route path="/Volume/Comment" element={<Comment />} />
+                <Route path="/Volume/Profile" element={<Profile />} />
+                <Route path="/Volume/CalcularPresupuesto" element={<Budget />} />
+                <Route path="/Volume/tutorials" element={<Tutorials />} />
+                <Route path="/Volume/tutorials/:id" element={<TutorialHandler/>} />
+                <Route path="/Volume/authors/:username" element={<AuthorsProfile />} />
+                <Route path="/Front-Eco3DPrint/InfoModel" element={<InfoModel />} />
+              </Routes>
+            </div>
+        </Router>
+      </div>
+>>>>>>> Stashed changes
   );
 }
 
--- src/views/STLVisualizer.jsx
+++ src/views/STLVisualizer.jsx
@@ -1,7 +1,13 @@
 import { useState, useRef } from "react";
 import React from "react";
 import { StlViewer } from "react-stl-viewer";
+<<<<<<< Updated upstream
 import { useLocation, useNavigate } from 'react-router-dom';
+=======
+import { useLocation, useNavigate } from "react-router-dom";
+import Comments from "./Comment";
+import ModelLikes from "./ModelLikesHandler";
+>>>>>>> Stashed changes
 import '../styles/VisualizarSTL.css'; 
 
 const style = {
@@ -14,7 +20,25 @@ const style = {
 
 export function VisualizarSTL() {
   const { state } = useLocation();
+<<<<<<< Updated upstream
   const url = state ? state + ".stl" : "Face.stl";
+=======
+  const url = state ? state.mainUrl : "thinker.stl";
+  const modelName = state ? state.modelName : "Nombre Predeterminado";
+  const modelId = 2; //SHOULD RETRIEVE IT FROM DB
+
+  const handleButtonClick = () => {
+    navigate(`/Front-Eco3DPrint/InfoModel?id=${modelId}`);
+  };
+
+  const downloadSTL = () => {
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = url.substring(url.lastIndexOf('/') + 1);
+    link.click();
+  };
+
+>>>>>>> Stashed changes
   const navigate = useNavigate();
   const modelId = 2; //SHOULD RETRIEVE IT FROM DB
 
@@ -30,6 +54,7 @@ export function VisualizarSTL() {
   };
 
   return (
+<<<<<<< Updated upstream
     <div style={style}>
       <StlViewer
         style={style}
@@ -42,12 +67,42 @@ export function VisualizarSTL() {
           positionY: 0,
         }}
       />
+=======
+    <div className="flex flex-col justify-center items-center">
+      <div className="w-[900px] h-[600px] bg-gray-300 mt-[300px] flex flex-col justify-center items-center">
+        <h2>{modelName}</h2>
+        <StlViewer
+          style={style}
+          orbitControls
+          shadows
+          url={url}
+          modelProps={{
+            color: "#0a6bc1",
+            positionX: 0,
+            positionY: 0,
+          }}
+        />
+      </div>
+
+>>>>>>> Stashed changes
       <div className="button-container">
         <button className="button-style" onClick={handleButtonClick}>
           Visualizar Información
         </button>
         <button className="button-style" onClick={downloadSTL}>
           Descargar modelo
+<<<<<<< Updated upstream
+=======
+        </button>
+      </div>
+
+      <div className="stl-viewer-container">
+        <button
+          className="mt-[100px]"
+          onClick={() => navigate("/Volume/authors/user1")}
+        >
+          Ir a perfil de usuario
+>>>>>>> Stashed changes
         </button>
       </div>
     </div>
