a9b6ea4b306d11d1d93b61621b350ce72477ce4a
Parbulitos <cechavarne@gmail.com>
2023-11-29 18:45:56 +0100

    Responsive

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -64,7 +64,7 @@ export default function SearchBar(){
                 </div>
             </div>
             <div id="catalogItemsContainer" className={`${blurOnSearch()} -mt-44 transition duration-300 relative z-0`}>
-                <div id="catalogGrid" className="grid grid-cols-4 gap-4 px-8 justify-between">
+                <div id="catalogGrid" className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 px-8">
                     <CatalogItem/>
                     <CatalogItem/>
                     <CatalogItem/>
