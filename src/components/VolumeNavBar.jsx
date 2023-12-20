ed35f0266b68794cf54bb0153a37118d2686b44f
Parbulitos <cechavarne@gmail.com>
2023-12-20 22:34:30 +0100

    LogoutButton

--- src/components/VolumeNavBar.jsx
+++ src/components/VolumeNavBar.jsx
@@ -3,10 +3,8 @@ import NavigationButton from "./NavigationButton.jsx";
 import {useNavigate} from "react-router-dom";
 import {useEffect, useState} from "react";
 
-export default function VolumeNavBar(){
+export default function VolumeNavBar() {
     const [isUserLoggedIn, setUserLoggedIn] = useState(false)
-    const [hoverProfile, setHoverProfile] = useState(false)
-    const [logoutEnabled, setLogoutEnabled] = useState(false)
     const volumeHomeUrl = '/volume/'
     const catalogUrl = '/volume/catalogo'
     const tutorialsUrl = '/volume/tutorials'
@@ -16,84 +14,67 @@ export default function VolumeNavBar(){
     const selfUser = JSON.parse(localStorage.getItem('user'));
 
     useEffect(() => {
-        if(user != null){
+        if (user != null) {
             setUserLoggedIn(true)
-        }else{
+        } else {
             setUserLoggedIn(false)
         }
     }, []);
 
     const navigate = useNavigate()
 
-    const navigateProfile = ()=>{
+    const navigateProfile = () => {
         navigate('/volume/profile', {
             state: {
                 user: user.username
             },
-          })
+        })
     }
 
-    const navigateUpload = ()=>{
+    const navigateUpload = () => {
         navigate('/volume/subirarchivo')
     }
 
-    const navigateLogin = ()=>{
+    const navigateLogin = () => {
         navigate('/volume/userlogin')
     }
 
-    const navigateSignIn = ()=>{
+    const navigateSignIn = () => {
         navigate('/volume/userregistration')
     }
 
-    const checkUserLogged = ()=>{
-        if(isUserLoggedIn){
+    const checkUserLogged = () => {
+        if (isUserLoggedIn) {
             return "sm:block"
-        }else{
+        } else {
             return "sm:hidden"
         }
     }
 
-    const showLoginAndRegister = ()=>{
-        if(!isUserLoggedIn){
+    const showLoginAndRegister = () => {
+        if (!isUserLoggedIn) {
             return "sm:flex"
-        }else{
+        } else {
             return "sm:hidden"
-        }
-    }
-
-    const handleLogoutVisibilityIn = ()=>{
-        setHoverProfile(true)
-        setLogoutEnabled(true)
-        logoutVisibility()
-    }
-
-    const handleLogoutVisibilityOut = ()=>{
-        setHoverProfile(false)
-        setLogoutEnabled(false)
-        logoutVisibility()
-    }
-
-    const logoutVisibility = ()=>{
-        if(hoverProfile){
-            return "opacity-100"
-        }else{
-            return "opacity-0"
         }
     }
 
     const logoutUser = ()=>{
         localStorage.clear()
-        localStorage.clear()
         navigate(volumeHomeUrl)
     }
 
-    return(
-        <div id="navigationBar" className="w-[60%] min-w-[216px] lg:w-[935px] sticky top-0 px-8 py-2 flex border-[1px] border-black rounded-full items-center justify-between animate-fade-down animate-delay-200 backdrop-blur-sm">
+    return (
+        <div id="navigationBar"
+             className="w-[60%] min-w-[216px] lg:w-[935px] sticky top-0 px-8 py-2 flex border-[1px] border-black rounded-full items-center justify-between animate-fade-down animate-delay-200 backdrop-blur-sm">
             <div>
                 <div id="logoContainer" className="flex items-center gap-12 my-2">
                     <VolumeLogoSm urlNavigate={volumeHomeUrl}/>
-                    <svg id="menuLgIcon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 hidden sm:block lg:hidden cursor-pointer hover:text-cyan-600">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                    <svg id="menuLgIcon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                         strokeWidth={1.5} stroke="currentColor"
+                         className="w-6 h-6 hidden sm:block lg:hidden cursor-pointer hover:text-cyan-600">
+                        <path strokeLinecap="round" strokeLinejoin="round"
+                              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>
                     </svg>
 
                     <NavigationButton name="Catálogo" urlNavigate={catalogUrl}/>
@@ -110,40 +91,45 @@ export default function VolumeNavBar(){
             <div className="flex items-center gap-4">
                 {!isLogged ? (<>
                     <div className={"flex gap-4 items-center"}>
-                    <p className="hover:underline transition duration-300 cursor-pointer text-sm" onClick={navigateLogin}>
-                        Iniciar sesión
-                    </p>
-                    <p className="text-lg cursor-default">|</p>
-                    <p className="hover:underline transition duration-300 cursor-pointer text-sm" onClick={navigateSignIn}>
-                        Registrarse
-                    </p>
-                </div>
-                </>) : (<>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={"w-8 h-8 hover:text-cyan-600 cursor-pointer hidden sm:block"} onClick={navigateUpload}>
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
-                </svg>
-                <div id="profileImgContainer"  onMouseLeave={handleLogoutVisibilityOut} className={"w-10 h-10 relative rounded-full bg-slate-800 hidden sm:block cursor-pointer"} >
-                    <img onMouseEnter={handleLogoutVisibilityIn} className="w-full h-full rounded-full" src={selfUser.imageUrl || "../default_image.png"} alt="PorfileImg" onClick={navigateProfile}/>
-                    <div id="logoutButton" className={`${logoutVisibility()} absolute -ml-7 border border-slate-600 px-1 py-1 rounded-lg bg-slate-100 opacity-0 transition duration-300`}>
-                        <button disabled={!logoutEnabled} className={`flex text-red-600 hover:bg-red-500 hover:text-white transition duration-300 rounded-lg px-4 py-1`} onClick={logoutUser}>
-                            Salir
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                 stroke="currentColor" data-slot="icon" className="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                      d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9"/>
-                            </svg>
-                        </button>
+                        <p className="hover:underline transition duration-300 cursor-pointer text-sm"
+                           onClick={navigateLogin}>
+                            Iniciar sesión
+                        </p>
+                        <p className="text-lg cursor-default">|</p>
+                        <p className="hover:underline transition duration-300 cursor-pointer text-sm"
+                           onClick={navigateSignIn}>
+                            Registrarse
+                        </p>
                     </div>
-                </div>
+                </>) : (<>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                         stroke="currentColor" className={"w-8 h-8 hover:text-cyan-600 transition duration-300 cursor-pointer hidden sm:block"}
+                         onClick={navigateUpload}>
+                        <path strokeLinecap="round" strokeLinejoin="round"
+                              d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"/>
+                    </svg>
+                    <div id="profileImgContainer"
+                         className={"w-10 h-10 rounded-full bg-slate-800 hidden sm:block cursor-pointer hover:drop-shadow-slim transition duration-300"}
+                         onClick={navigateProfile}>
+                        <img className="w-full h-full rounded-full group-hover:opacity-50"
+                             src={selfUser.imageUrl || "../default_image.png"}/>
+                    </div>
+                    <div id="logoutButtonContainer" className="has-tooltip" onClick={logoutUser}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                             stroke="currentColor" data-slot="icon" className="w-8 h-8 rounded-full hover:stroke-red-500 transition duration-300 cursor-pointer">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                  d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9"/>
+                        </svg>
+                    </div>
                     <svg id="menuIcon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                          strokeWidth={1.5} stroke="currentColor"
                          className="w-5 h-5 ml-2 sm:hidden cursor-pointer hover:text-cyan-600">
                         <path strokeLinecap="round" strokeLinejoin="round"
-                              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                </svg>
+                              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>
+                    </svg>
                 </>)}
-                
-                
+
+
             </div>
         </div>
     )
